318676c0c02d6598d9f8953f7d81c226c0ec2c89
Noam Domovich <ndomovich@gmail.com>
2021-02-24 14:31:34 +0200

    Playing by pad status

--- src/components/AudioBoard.js
+++ src/components/AudioBoard.js
@@ -1,12 +1,20 @@
+import React, { useState  } from 'react';
+import { Button } from 'react-bootstrap'
+
+
 import Pad from './Pad.js'
-import { CardGroup } from 'react-bootstrap'
-const path = require('path');
 
 const AudioBoard = () => {
-    const tracks = ['song1', 'song2', 'song3'];
+    const [playingState, setPlayingState] = useState(false);
 
-    const directoryPath = path.join(__dirname, 'assets');
 
+
+    // const tracks = ['120_future_funk_beats_25.mp3'];
+    const tracks = ['120_future_funk_beats_25.mp3', '120_stutter_breakbeats_16.mp3', 'Bass Warwick heavy funk groove on E 120 BPM.mp3'];
+
+    const handleOnClick = () => {
+        setPlayingState(!playingState);
+    }
 
 
     return (
@@ -17,11 +25,14 @@ const AudioBoard = () => {
             <div>
                 {tracks.map((track, idx) => {
                     return (
-                        <Pad key={idx} title={track} />
+                        <Pad key={idx} title={track} playingState={playingState} />
                     )
                 })
 
                 }
+            </div>
+            <div className='text-center'>
+                <Button variant={playingState ? "danger" : "success"} className="mx-1" onClick={handleOnClick}>{playingState ? "Pause" : "Play"}</Button>
             </div>
         </div>
     );
--- src/components/Pad.js
+++ src/components/Pad.js
@@ -1,25 +1,62 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Button } from 'react-bootstrap'
 import { Card } from 'react-bootstrap';
 
+const path = require('path');
 
 
-export default function Pad({ title }) {
+export default function Pad({ title, playingState }) {
+    const trackPath = path.join('assets', title);
+    const [padState, setPadState] = useState(false)
+    const audioRef = useRef(null);
+
+    // useEffect(() => {
+    //     if (padState ) {
+    //         audioRef.current.pause();
+    //     } else {
+    //         audioRef.current.play();
+    //     }
+    // }, [padState]);
+
+
+    useEffect(() => {
+        if(playingState && padState) {
+            audioRef.current.play();
+        } else {
+            audioRef.current.pause();
+
+        }
+
+    }, [playingState , padState]);
+
+
+    const handleOnClick = () => {
+        setPadState(!padState);
+    }
+
+
 
 
     return (
-
-
         <Card
             bg={'primary'}
             text={'white'}
-            style={{ width: '18rem' }}
-            className="mb-2"
+            style={{ width: '50%' }}
+            className="mx-auto my-2 text-center"
         >
             <Card.Body>
-                    
+                <audio src={"/assets/120_future_funk_beats_25.mp3"} ref={audioRef} loop />
+                <Card.Text>
+                    {title}
+                </Card.Text>
 
-                    <audio src={"/assets/120_future_funk_beats_25.mp3"} controls  autoPlay  loop />
+
 
             </Card.Body>
+            <Card.Footer>
+                <Button variant={padState ? "danger" : "success"} className="mx-1" onClick={handleOnClick}>{padState ? "off" : "on"}</Button>
+                {/* <Button variant="danger" className="mx-1">Off</Button> */}
+            </Card.Footer>
         </Card>
     );
 }
