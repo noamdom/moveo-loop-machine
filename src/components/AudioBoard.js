566f2d678ea2db86a07c709ee4e755da9e128686
Noam Domovich <ndomovich@gmail.com>
2021-02-24 09:49:12 +0200

    first commit

--- src/components/AudioBoard.js
+++ src/components/AudioBoard.js
@@ -1,25 +1,11 @@
 import Pad from './Pad.js'
 import { CardGroup } from 'react-bootstrap'
-const fs = require('fs');
 const path = require('path');
 
 const AudioBoard = () => {
     const tracks = ['song1', 'song2', 'song3'];
 
     const directoryPath = path.join(__dirname, 'assets');
-
-
-    fs.readdir(directoryPath, function (err, files) {
-        //handling error
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        }
-        //listing all files using forEach
-        files.forEach(function (file) {
-            // Do whatever you want to do with the file
-            console.log(file);
-        });
-    });
 
 
 
